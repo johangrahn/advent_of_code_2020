65372e1e7323281a8f58d413941921db4f70f3a3
Johan Grahn <johan.grahn@me.com>
2021-05-09 19:53:18 +0200

    Add day3 part 2

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,24 +1,61 @@
 pub fn day3(input: String) -> (usize, usize) {
-    (part1(&"".to_string()), 0)
-}
-
-pub fn part1(input: &String) -> usize {
     let data = input
         .lines()
         .map(|l| l.trim().into())
         .collect::<Vec<String>>();
-    let length = data.len();
+
+    (part1(&data), part2(&data))
+}
+
+pub fn part1(data: &Vec<String>) -> usize {
     let column_length = data[0].len();
-    let mut column = 0;
+
+    calc_trees(&data, column_length, 1, 3)
+}
+
+pub fn part2(data: &Vec<String>) -> usize {
+    let column_length = data[0].len();
+    let slopes = vec![(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)];
+    slopes
+        .iter()
+        .map(|slope| calc_trees(data, column_length, slope.0, slope.1))
+        .product::<usize>()
+}
+
+fn calc_trees(
+    data: &Vec<String>,
+    column_length: usize,
+    row_jump: usize,
+    column_jump: usize,
+) -> usize {
+    let mut row = row_jump;
+    let mut column = column_jump;
     let mut count = 0;
-    for index in 1..length {
-        column = (column + 3) % column_length;
-        let c = data[index].chars().nth(column).unwrap();
-        println!("Char found:{}", c);
-        if c == '#' {
-            count += 1;
+
+    // for row in row_jump..length {
+    //     column = (column + column_jump) % column_length;
+    //     let c = data[row].chars().nth(column).unwrap();
+    //     if c == '#' {
+    //         count += 1;
+    //     }
+    // }
+
+    println!("Calculating: {},{}", row, column);
+    loop {
+        if let Some(line) = data.get(row) {
+            let c = line.chars().nth(column).unwrap();
+            if c == '#' {
+                count += 1;
+            }
+
+            column = (column + column_jump) % column_length;
+            row += row_jump
+        } else {
+            break;
         }
     }
+
+    println!("Found trees: {}", count);
     count
 }
 
@@ -38,14 +75,41 @@ mod part1 {
 #...##....#
 .#..#...#.#"
             .to_string();
-        let result = crate::days::day3::part1(&input);
-        assert_eq!(result, 7);
+        let result = crate::days::day3::day3(input);
+        assert_eq!(result.0, 7);
     }
 
     #[test]
     fn input() {
         let input = std::fs::read_to_string("input/3.txt").unwrap();
-        let result = crate::days::day3::part1(&input);
-        assert_eq!(result, 214);
+        let result = crate::days::day3::day3(input);
+        assert_eq!(result.0, 214);
+    }
+}
+
+mod part2 {
+    #[test]
+    fn example() {
+        let input = "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"
+            .to_string();
+        let result = crate::days::day3::day3(input);
+        assert_eq!(result.1, 336);
+    }
+
+    #[test]
+    fn input() {
+        let input = std::fs::read_to_string("input/3.txt").unwrap();
+        let result = crate::days::day3::day3(input);
+        assert_eq!(result.1, 8336352024);
     }
 }
